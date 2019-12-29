4cd344fc1b3a50f0ba0e90a0e17efbe896367347
eddiecarbin <paullaster@yahoo.com>
2019-12-28 16:40:04 -0800

    PCB

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+int ledPin = 9;    // LED connected to digital pin 9
+int analogPin = 3; // potentiometer connected to analog pin 3
+int val = 0;       // variable to store the read value
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  // pinMode(ledPin, OUTPUT); // sets the pin as output
+  // pinMode(analogPin, INPUT);
+
+  pinMode(ledPin, INPUT_PULLUP); 
+  analogWrite(ledPin, 100);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  val = analogRead(ledPin);  // read the input pin
+  
+  Serial.println(val);
+
+  //analogWrite(ledPin, val / 4); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
 }
